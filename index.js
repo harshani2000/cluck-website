958f2e5bada6eea186ab1c6d0e20c32752f5e4e8
kushan2k <kushangayantha001@gmail.com>
2023-08-16 11:50:17 +0530

    Added: Insertion observer added

---
+++ index.js
@@ -0,0 +1,18 @@
+function createObserver(itemClassName, animationClassName) {
+  let observer = new IntersectionObserver(
+    function (entries, observer) {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle(animationClassName, entry.isIntersecting)
+      })
+    },
+    {
+      threshold: 0.5,
+    }
+  )
+
+  document.querySelectorAll(`.${itemClassName}`).forEach((ele) => {
+    observer.observe(ele)
+  })
+}
+
+module.exports = createObserver
